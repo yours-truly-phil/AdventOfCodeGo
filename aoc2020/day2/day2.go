58f0a421e6d7785331ebf031ae6f2b32758a2da8
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-12 17:01:27 +0100

    Day 2 Part 2 AoC 2020

--- aoc2020/day2/day2.go
+++ aoc2020/day2/day2.go
@@ -8,19 +8,30 @@ import (
 	"strings"
 )
 
-func part1(path string) int {
+func linesScanner(path string) *bufio.Scanner {
 	file, _ := os.Open(path)
 	scanner := bufio.NewScanner(bufio.NewReader(file))
 	scanner.Split(bufio.ScanLines)
+	return scanner
+}
+
+func parseLine(line string) (int, int, uint8, string) {
+	parts := strings.Split(line, " ")
+	nums := strings.Split(parts[0], "-")
+	low, _ := strconv.Atoi(nums[0])
+	high, _ := strconv.Atoi(nums[1])
+	char := parts[1][0]
+	pw := parts[2]
+	return low, high, char, pw
+}
+
+func part1(path string) int {
+	scanner := linesScanner(path)
 	var countValid = 0
 	for scanner.Scan() {
 		s := scanner.Text()
-		parts := strings.Split(s, " ")
-		nums := strings.Split(parts[0], "-")
-		low, _ := strconv.Atoi(nums[0])
-		high, _ := strconv.Atoi(nums[1])
-		char := string(parts[1][0])
-		freq := strings.Count(parts[2], char)
+		low, high, char, pw := parseLine(s)
+		freq := strings.Count(pw, string(char))
 		if freq >= low && freq <= high {
 			countValid++
 		}
@@ -28,10 +39,20 @@ func part1(path string) int {
 	return countValid
 }
 
-func part2(path string) {
-
+func part2(path string) int {
+	scanner := linesScanner(path)
+	var countValid = 0
+	for scanner.Scan() {
+		s := scanner.Text()
+		low, high, char, pw := parseLine(s)
+		if (pw[low-1] == char || pw[high-1] == char) && pw[low-1] != pw[high-1] {
+			countValid++
+		}
+	}
+	return countValid
 }
 
 func main() {
 	fmt.Println(part1("aoc2020/day2/day2.txt"))
+	fmt.Println(part2("aoc2020/day2/day2.txt"))
 }
